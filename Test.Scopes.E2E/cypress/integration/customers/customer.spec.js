ef112bdd54477dd69cc81a63a446f51b0347bfc3
Alex Alves <alexalves2501@hotmail.com>
2022-04-18 10:03:35 -0300

    Include integration tests

---
+++ Test.Scopes.E2E/cypress/integration/customers/customer.spec.js
@@ -0,0 +1,47 @@
+/// <reference types="cypress" />
+
+context('Customers', () => {
+    beforeEach(() => {
+        cy.visit('https://localhost:7184/customer')
+    })
+
+    describe('Verify customers registered', () => {
+        it('Contains 5 customers in list', () => verifyList(5))
+    })
+
+    describe('Register customers', () => {
+        beforeEach(() => cy.get('#create_customer').click())
+
+        it('With a valid fields', () => {
+            cy.get('#login').type('joaodasilva')
+            cy.get('#password').type('12345678')
+            cy.get('#name').type('João da Silva')
+            cy.get('#birth_date').type('1990-02-01')
+            cy.get('#email').type('joao@gmail.com')
+            cy.get('#phone').type('(19) 9880-0102')
+
+            cy.get('#create_customer_modal').click()
+
+            verifyList(6)
+        })
+
+        it('With invalid name', () => {
+            cy.get('#login').type('joaodasilva')
+            cy.get('#password').type('12345678')
+            cy.get('#name').type('JP')
+            cy.get('#birth_date').type('1990-02-01')
+            cy.get('#email').type('joao@gmail.com')
+            cy.get('#phone').type('(19) 9880-0102')
+
+            cy.get('#create_customer_modal').click()
+
+            verifyList(6)
+        })
+    })
+})
+
+function verifyList(numberOfItens) {
+    cy.get('.table > tbody')
+        .find('tr')
+        .should('have.length', numberOfItens)
+}
